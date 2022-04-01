b554fa704f773ba1610e3b88868724fa11161820
Eslam <eslam.farouk.mh@gmail.com>
2022-04-01 03:23:40 +0200

    fix in ui sample to match the givin one

--- src/app/shared/converter-panel/converter-panel.component.ts
+++ src/app/shared/converter-panel/converter-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-converter-panel',
